2cf864c1551b974b62345b5105f839998bdcde45
Lvov Pavel <pasha-lvovp@mail.ru>
2025-08-02 13:00:45 +0300

    The descriptions of functions and their adaptation to your project have been updated

--- Core/Inc/reciever.h
+++ Core/Inc/reciever.h
@@ -1,23 +1,49 @@
 /*
  * reciever.h
+ * Библиотека для приема 6-канальных ШИМ-сигналов
+ * Особенности:
+ *   - Поддержка трех таймеров
+ *   - Автоматическое определение периода и ширины импульса
+ *   - Таймаут при потере сигнала
+ *   - При пропадании сигнала с одного канала таймера обнуляются оба значения ШИМ сигналов приходящих на этот таймер
  *
- *  Created on: Jul 30, 2025
- *      Author: pasha
+ * Версия: 1.1
+ * Автор: Pavel Lvov
+ * Дата создания: 30.07.2025
  */
 
 #ifndef INC_RECIEVER_H_
 #define INC_RECIEVER_H_
 
-#define SIGNAL_TIMEOUT_MS 100
+/* Для каждого таймера (сама система настрена на 72 МГц, тогда получаем значения в мкс):
+ * Prescaler: 72-1 (для 1 МГц тактовой частоты)
+ * Counter Period: 65535
+ * Clock Source: Internal Clock
+ * TIMx global interrupt: Enable
+ * Режим CH1 или CH3: Input Capture direct mode
+ * Режим CH2 или CH4: Input Capture indirect mode
+ * Канал 1: Rising Edge
+ * Канал 2: Falling Edge
+ * Канал 3: Rising Edge
+ * Канал 4: Falling Edge
+ * !!! подтягиваем ножки ввода к земле (GPIO: Pull-down) !!!
+ *
+ */
 
-#define TIMER1_OFFSET 16
-#define TIMER2_OFFSET 25
-#define TIMER3_OFFSET 7
+//-------------------------------------DEFINES-------------------------------------//
+#define SIGNAL_TIMEOUT_MS 100 				// Таймаут потери сигнала (мс)
 
-#define TIMER1 TIM2
-#define TIMER2 TIM3
-#define TIMER3 TIM4
+// Настройка используемых таймеров (ИЗМЕНИТЕ ПОД ВАШУ КОНФИГУРАЦИЮ)
+#define TIMER1 TIM2  						// Таймер для каналов 1-2
+#define TIMER2 TIM3  						// Таймер для каналов 3-4
+#define TIMER3 TIM4  						// Таймер для каналов 5-6
 
+// Калибровочные смещения для каждого таймера (корректировка погрешности измерения)
+#define TIMER1_OFFSET 16  					// Смещение для TIMER1
+#define TIMER2_OFFSET 25  					// Смещение для TIMER2
+#define TIMER3_OFFSET 7   					// Смещение для TIMER3
+
+// задайте свои переменные
 #define CH1_LAST_UPDATE ch1_last_update
 #define CH2_LAST_UPDATE ch2_last_update
 #define CH3_LAST_UPDATE ch3_last_update
@@ -39,25 +65,41 @@
 #define CH5_PULSE ch5_pulse
 #define CH6_PULSE ch6_pulse
 
-// !!!
-// !!!
-//определите свои объекты таймеров
+//-------------------------------------VARIABLES-------------------------------------//
+//определите объекты своих таймеров
 extern TIM_HandleTypeDef htim2;
 extern TIM_HandleTypeDef htim3;
 extern TIM_HandleTypeDef htim4;
 
-extern volatile uint32_t CH1_PERIOD, CH1_PULSE;
-extern volatile uint32_t CH2_PERIOD, CH2_PULSE;
-extern volatile uint32_t CH3_PERIOD, CH3_PULSE;
-extern volatile uint32_t CH4_PERIOD, CH4_PULSE;
-extern volatile uint32_t CH5_PERIOD, CH5_PULSE;
-extern volatile uint32_t CH6_PERIOD, CH6_PULSE;
-extern volatile uint32_t CH1_LAST_UPDATE, CH2_LAST_UPDATE, CH3_LAST_UPDATE, CH4_LAST_UPDATE, CH5_LAST_UPDATE, CH6_LAST_UPDATE;
+// Глобальные переменные для доступа к данным ШИМ (обновляются автоматически)
+extern volatile uint32_t CH1_PERIOD, CH1_PULSE;	// Канал 1: период (мкс), ширина импульса (мкс)
+extern volatile uint32_t CH2_PERIOD, CH2_PULSE;	// Канал 2: период (мкс), ширина импульса (мкс)
+extern volatile uint32_t CH3_PERIOD, CH3_PULSE;	// Канал 3: период (мкс), ширина импульса (мкс)
+extern volatile uint32_t CH4_PERIOD, CH4_PULSE;	// Канал 4: период (мкс), ширина импульса (мкс)
+extern volatile uint32_t CH5_PERIOD, CH5_PULSE; // Канал 5: период (мкс), ширина импульса (мкс)
+extern volatile uint32_t CH6_PERIOD, CH6_PULSE; // Канал 6: период (мкс), ширина импульса (мкс)
+// Время последнего обновления (для проверки таймаута)
+extern volatile uint32_t CH1_LAST_UPDATE, CH2_LAST_UPDATE, CH3_LAST_UPDATE;
+extern volatile uint32_t CH4_LAST_UPDATE, CH5_LAST_UPDATE, CH6_LAST_UPDATE;
 
-void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim);
+//-------------------------------------FUNCTIONS-------------------------------------//
+/**
+ * @brief Инициализация каналов приема
+ * @note Должна вызываться после инициализации таймеров
+ */
 void Rec_Channel_Init(void);
 
-//
+/**
+ * @brief Стандартная функция библиотеки HAL
+ * @note вызывается после обработчика прерывания
+ */
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim);
+
+/**
+ * @brief Проверка актуальности сигналов
+ * @note Автоматически обнуляет каналы при превышении таймаута
+ *       Должна вызываться периодически (например, в основном цикле)
+ */
 void check_signal (void);
 
 #endif /* INC_RECIEVER_H_ */
--- Core/Src/reciever.c
+++ Core/Src/reciever.c
@@ -1,35 +1,69 @@
 /*
  * reciever.c
+ * Реализация приема 6-канальных ШИМ-сигналов
+ * Особенности:
+ *   - Использование режима Input Capture
+ *   - Автоматический сброс при потере сигнала
+ *   - Корректировка погрешности измерения через смещения
  *
- *  Created on: Jul 30, 2025
- *      Author: Pavel Lvov
+ * Версия: 1.1
+ * Автор: Pavel Lvov
+ * Дата создания: 30.07.2025
  */
 #include "main.h"
 #include "reciever.h"
 
-
+//-------------------------------------GLOBAL VARIABLS-------------------------------------//
+// Значения ШИМ для каждого канала (обновляются в прерываниях)
 volatile uint32_t CH1_PERIOD = 0, CH1_PULSE = 0;
 volatile uint32_t CH2_PERIOD = 0, CH2_PULSE = 0;
 volatile uint32_t CH3_PERIOD = 0, CH3_PULSE = 0;
 volatile uint32_t CH4_PERIOD = 0, CH4_PULSE = 0;
 volatile uint32_t CH5_PERIOD = 0, CH5_PULSE = 0;
 volatile uint32_t CH6_PERIOD = 0, CH6_PULSE = 0;
-volatile uint32_t CH1_LAST_UPDATE = 0, CH2_LAST_UPDATE = 0, CH3_LAST_UPDATE = 0, CH4_LAST_UPDATE = 0, CH5_LAST_UPDATE = 0, CH6_LAST_UPDATE = 0;
 
+// Время последнего обновления (системные тики)
+volatile uint32_t CH1_LAST_UPDATE = 0, CH2_LAST_UPDATE = 0, CH3_LAST_UPDATE = 0;
+volatile uint32_t CH4_LAST_UPDATE = 0, CH5_LAST_UPDATE = 0, CH6_LAST_UPDATE = 0;
+
+//-------------------------------------FUNCTIONS-------------------------------------//
+/**
+ * @brief Инициализация каналов приема ШИМ
+ * @note Запускает захват сигналов на всех каналах
+ *       Для каналов 1,3,5 включены прерывания (период)
+ *       Для каналов 2,4,6 используется прямой захват (ширина импульса)
+ *       (запускаем нужные каналы на выбранных таймерах (каналы работают только в паре 1-2 и 3-4)
+ */
 void Rec_Channel_Init(void){
+	  // Таймер 1 - сигналы 1-2
 	  HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_1);
 	  HAL_TIM_IC_Start(&htim2, TIM_CHANNEL_2);
 	  HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_3);
 	  HAL_TIM_IC_Start(&htim2, TIM_CHANNEL_4);
+
+	  // Таймер 2 - сигналы 3-4
 	  HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_1);
 	  HAL_TIM_IC_Start(&htim3, TIM_CHANNEL_2);
 	  HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_3);
 	  HAL_TIM_IC_Start(&htim3, TIM_CHANNEL_4);
+
+	  // Таймер 3 - сигналы 5-6
 	  HAL_TIM_IC_Start_IT(&htim4, TIM_CHANNEL_1);
 	  HAL_TIM_IC_Start(&htim4, TIM_CHANNEL_2);
 	  HAL_TIM_IC_Start_IT(&htim4, TIM_CHANNEL_3);
 	  HAL_TIM_IC_Start(&htim4, TIM_CHANNEL_4);
 }
+
+
+/**
+ * @brief Callback-функция захвата сигнала
+ * @param htim Указатель на обработчик таймера
+ * @note Автоматически вызывается при захвате фронта сигнала
+ * 		для адаптации кода нужно изменить макросы таймеров и !
+ * 		! изменить значения регистров (TIM_SR_CCxIF и ССRx) в зависимости от используемых каналов:
+ * 		если используется пара 1-2 то TIM_SR_CC1IF и CCR1 + CCR2, если 3-4 -- TIM_SR_CC3IF и CCR3 + CCR4
+ *
+ * 		так же если используется только каналы 1-2, то необходимо раскоментировать строчки /* */  //кроме того что в условии if()
 
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 {
@@ -38,16 +72,16 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
     	uint32_t now1 = HAL_GetTick();
     	if (/*htim->Instance->SR & TIM_SR_CC1IF*/ 1) {
     		htim->Instance->SR &= ~TIM_SR_CC1IF;
-    		//htim->Instance->CNT = 0; //сброс счетчика
+    		/*htim->Instance->CNT = 0;*/ 		//сброс счетчика
     	    // Signal 1: Чтение периода (CCR1) и ширины импульса (CCR2)
     	    CH1_PERIOD = (htim->Instance->CCR1) + TIMER1_OFFSET;
     	    CH1_PULSE = (htim->Instance->CCR2) + TIMER1_OFFSET;
-    	    //ch1_last_update = now1;
+    	    /*CH1_LAST_UPDATE = now1;*/
     	}
     	if (htim->Instance->SR & TIM_SR_CC3IF) {
     		htim->Instance->SR &= ~TIM_SR_CC3IF;
-    		htim->Instance->CNT = 0; //сброс счетчика
-    	    // Signal 1: Чтение периода (CCR1) и ширины импульса (CCR2)
+    		htim->Instance->CNT = 0; 			//сброс счетчика
+    	    // Signal 2: Чтение периода (CCR3) и ширины импульса (CCR4)
     		CH2_PERIOD = (htim->Instance->CCR3) + TIMER1_OFFSET;
     		CH2_PULSE = (htim->Instance->CCR4) + TIMER1_OFFSET;
     		CH2_LAST_UPDATE = now1;
@@ -58,16 +92,16 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 		uint32_t now2 = HAL_GetTick();
 		if (/*htim->Instance->SR & TIM_SR_CC1IF*/ 1) {
 			htim->Instance->SR &= ~TIM_SR_CC1IF;
-			//htim->Instance->CNT = 0; //сброс счетчика
+			/*htim->Instance->CNT = 0;*/ 		//сброс счетчика
 			// Signal 1: Чтение периода (CCR1) и ширины импульса (CCR2)
 			CH3_PERIOD = (htim->Instance->CCR1) + TIMER2_OFFSET;
 			CH3_PULSE = (htim->Instance->CCR2) + TIMER2_OFFSET;
-			//CH3_LAST_UPDATE = now2;
+			/*CH3_LAST_UPDATE = now2;*/
 		}
 		if (htim->Instance->SR & TIM_SR_CC3IF) {
 			htim->Instance->SR &= ~TIM_SR_CC3IF;
-			htim->Instance->CNT = 0; //сброс счетчика
-			// Signal 1: Чтение периода (CCR1) и ширины импульса (CCR2)
+			htim->Instance->CNT = 0; 			//сброс счетчика
+			// Signal 2: Чтение периода (CCR3) и ширины импульса (CCR4)
 			CH4_PERIOD = (htim->Instance->CCR3) + TIMER2_OFFSET;
 			CH4_PULSE = (htim->Instance->CCR4) + TIMER2_OFFSET;
 			CH4_LAST_UPDATE = now2;
@@ -78,16 +112,16 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 		uint32_t now3 = HAL_GetTick();
 		if (/*htim->Instance->SR & TIM_SR_CC1IF*/ 1) {
 			htim->Instance->SR &= ~TIM_SR_CC1IF;
-			//htim->Instance->CNT = 0; //сброс счетчика
+			/*htim->Instance->CNT = 0;*/ 		//сброс счетчика
 			// Signal 1: Чтение периода (CCR1) и ширины импульса (CCR2)
 			CH5_PERIOD = (htim->Instance->CCR1) + TIMER3_OFFSET;
 			CH5_PULSE = (htim->Instance->CCR2) + TIMER3_OFFSET;
-			//CH5_LAST_UPDATE = now3;
+			/*CH5_LAST_UPDATE = now3;*/
 		}
 		if (htim->Instance->SR & TIM_SR_CC3IF) {
 			htim->Instance->SR &= ~TIM_SR_CC3IF;
-			htim->Instance->CNT = 0; //сброс счетчика
-			// Signal 1: Чтение периода (CCR1) и ширины импульса (CCR2)
+			htim->Instance->CNT = 0; 			//сброс счетчика
+			// Signal 2: Чтение периода (CCR3) и ширины импульса (CCR4)
 			CH6_PERIOD = (htim->Instance->CCR3) + TIMER3_OFFSET;
 			CH6_PULSE = (htim->Instance->CCR4) + TIMER3_OFFSET;
 			CH6_LAST_UPDATE = now3;
@@ -95,6 +129,11 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 	}
 }
 
+/**
+ * @brief Проверка актуальности сигналов
+ * @note Обнуляет значения каналов при превышении таймаута
+ *       Должна вызываться периодически (рекомендуется >10 Гц)
+ */
 void check_signal (void){
 	uint32_t recently_time = HAL_GetTick();
 	if ((recently_time - CH2_LAST_UPDATE) > SIGNAL_TIMEOUT_MS) {
